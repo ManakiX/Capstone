bc442a6bded833b2301bfb02a71998354f186ce9
ManakiX <sogeyanaluntu@gmail.com>
2023-03-13 15:42:21 +0200

    wait

--- frontend/src/components/FooterC.vue
+++ frontend/src/components/FooterC.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
+    <div class="footer__container">
+      <div class="footer__row">
+        <div class="footer__column">
           <h5>About Me</h5>
           <ul>
             <li><a href="#">My Story</a></li>
@@ -10,7 +10,7 @@
           </ul>
         </div>
 
-        <div class="col-md-6">
+        <div class="footer__column">
           <h5>Follow Me</h5>
           <ul>
             <li><a href="#">Facebook</a></li>
@@ -19,9 +19,9 @@
           </ul>
         </div>
       </div>
-      <hr>
-      <div class="row">
-        <div class="col-12">
+      <hr class="footer__divider">
+      <div class="footer__row">
+        <div class="footer__column">
           <p>&copy; 2023 Archie-Vable Gaming. All rights reserved.</p>
         </div>
       </div>
@@ -42,28 +42,46 @@ export default{
   text-align: center;
 }
 
-.footer h5{
+.footer__container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+
+.footer__row {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.footer__column {
+  flex: 1;
+  max-width: 300px;
+  margin: 0 20px;
+}
+
+.footer__column h5{
   color: #fff;
   font-size: 18px;
   margin-bottom: 25px;
 }
 
-.footer ul li a{
+.footer__column ul li a{
   color: #fff;
   font-size: 14px;
   line-height: 24px;
 }
 
-.footer hr{
+.footer__divider {
   border-color: #fff;
   margin: 40px 0;
 }
 
-.footer p{
+.footer__column p{
   color: grey;
   font-size: 14px;
   line-height: 24px;
 }
 </style>
+
 
   
--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,14 +1,36 @@
-<template lang="">
+<template>
     <div>
-        
+        <table class="table table-hover table-striped table-dark">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products" :key="product">
+                    <td>{{ product.id }}</td>
+                    <td>{{ product.prodDescription }}</td>
+                    <td>R{{ product.price }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
+
 <script>
-export default {
-    
+import { useStore } from 'vuex';
+import {computed} from '@vue/runtime-core'
+export default{
+    setup(){
+        const store = useStore();
+        store.dispatch('fetchProducts');
+        let products = computed(() => store.state.products);
+        return{
+            products
+        }
+    }
 }
 </script>
-<style lang="">
-    
-</style>
